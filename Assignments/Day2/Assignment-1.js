542f39b4b1312b3478248f8dde3e33e67a2b15cb
= <=>
2025-11-27 12:57:13 +0530

    Uploading Day3 Assignments

---
+++ Assignments/Day2/Assignment-1.js
@@ -0,0 +1,26 @@
+let  genderType = "Female";
+
+function printGender() {
+
+    let color = "brown";
+        
+    
+
+    if(genderType.startsWith("Female")) {
+
+            var age = 30;
+            let color = "Pink";
+
+            console.log(color);  // Pink
+
+    } else{
+
+        var age =35;
+        console.log(color);  // brown
+    }
+
+    console.log(age);  // 30
+
+    }
+
+    printGender()
