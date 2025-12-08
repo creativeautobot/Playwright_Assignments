7fe7b0b828cc54e272715321817e509e3760e779
= <=>
2025-12-08 14:58:02 +0530

    2nd week Assignments

---
+++ Assignments/Day5/NewJsFunctions.js
@@ -0,0 +1,18 @@
+//Task1
+function userProfile(name){
+
+    console.log("Hello, " + name+"!");
+
+
+}
+
+//Task2
+let double = (Double)=>{
+
+    return Double * 2;
+}
+
+console.log(double(10));
+userProfile("Rajsekhar");
+
+//Task3
